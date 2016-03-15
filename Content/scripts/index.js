0f4c9006f74565899b7d1c1a63dbe3cc65c69b70
Dzmitry Matus <dzmitry_matus@epam.com>
2016-03-15 12:30:15 +0300

    Initial commit

---
+++ Content/scripts/index.js
@@ -0,0 +1,46 @@
+﻿var pizzas;
+var paginator;
+$(document).ready(function ()
+{
+    $("#serach-button").click(search);
+    var url = "pizzas.json";
+    $.getJSON(url, null, function (data) 
+    {
+        pizzas = data;
+
+        $("#pagination").twbsPagination({
+            totalPages: Math.round(data.length / 6.0 + 0.49),
+            onPageClick: function (event, page) {
+                $("#pizzas-container").empty();
+                $("#pizzaTemplate").tmpl(data.slice(page * 6 - 6, page * 6 + 6 - 6)).appendTo("#pizzas-container");
+                $('main').animate({ scrollTop: "0px" }, 'slow');
+            }
+        });
+        $(".cssload-thecube").hide(400);
+        $("#pizzas-container").show(700);
+    });
+});
+
+function search()
+{
+    var result = [];
+    var searchValue = $("#keywords").val();
+    $.each(pizzas, function (index, value)
+    {
+        if (value.Name.toLowerCase().indexOf(searchValue.toLowerCase()) != -1 || value.Сomposition.toLowerCase().indexOf(searchValue.toLowerCase()) != -1)
+        {
+            result.push(value);
+        }
+    });
+
+    $("#pagination").twbsPagination('destroy');
+    $("#pagination").twbsPagination(
+        {
+            totalPages: Math.round(result.length / 6.0 + 0.49),
+            onPageClick: function (event, page) {
+                $("#pizzas-container").empty();
+                $("#pizzaTemplate").tmpl(result.slice(page * 6 - 6, page * 6 + 6 - 6)).appendTo("#pizzas-container");
+                $('main').animate({ scrollTop: "0px" }, 'slow');
+            }
+        });
+}
