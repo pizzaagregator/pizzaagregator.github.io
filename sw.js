ffe638b640c9b4f7fbb2c43d9ce0dee789312a8d
Dzmitry Matus <dzmitry_matus@epam.com>
2016-03-15 15:48:54 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -2,14 +2,13 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('pizzaagregator').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html',
-       '/pizzas.json',
-       '/Content/scripts',
-       '/Content/styles/site.css',
-       '/Content/styles/load-cube.css',
-       '/Content/styles/material.brown-blue.min.css',
-       '/scripts/main.min.js'
+       'index.html',
+       'pizzas.json',
+       'Content/scripts',
+       'Content/styles/site.css',
+       'Content/styles/load-cube.css',
+       'Content/styles/material.brown-blue.min.css',
+       'scripts/main.min.js'
      ]);
    })
  );
