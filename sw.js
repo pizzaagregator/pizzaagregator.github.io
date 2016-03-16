0b6b67c8d980e81aabf59ff19e958f231289b0f7
Dzmitry Matus <dzmitry_matus@epam.com>
2016-03-16 16:11:27 +0300

    edit service worker

---
+++ sw.js
@@ -0,0 +1,55 @@
+importScripts('/cache-polyfill.js');
+importScripts('/scripts/jquery-2.1.4.min.js');
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('pizzaagregator').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles/Site.css',
+        '/styles/load-cube.css',
+        '/styles/material.brown-blue.min.css',
+        'https://fonts.googleapis.com/css?family=Abel',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        '/scripts/',
+        '/pizzas.json'
+      ]).then(function() {
+        return self.skipWaiting();
+      });
+    })
+  );
+});
+
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+
+            caches.open('pizzaagregator')
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
