6d71f521ee91e5aaf90d0306d4abc1900fc8aaf9
Dzmitry Matus <dzmitry_matus@epam.com>
2016-03-18 12:46:52 +0300

    sw

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-        return fetch(event.request);
+        return fetch(changeImageUrl(event.request));
       }
     )
   );
