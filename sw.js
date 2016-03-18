ccd1a007c2ac9c9fcaf0d334bf675277521569b0
Dzmitry Matus <dzmitry_matus@epam.com>
2016-03-18 12:09:50 +0300

    sw

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-        return fetch(event.request,{mode: 'no-cors'});
+        return fetch(event.request,{mode: 'cors'});
       }
     )
   );
