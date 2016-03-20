4fd6088d77a5bad8de0a39449a3d536963f05314
dzmitrymatus <dzmitry_matus@epam.com>
2016-03-20 18:37:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,8 +13,7 @@ self.addEventListener('install', function(e) {
         '/Content/styles/material.brown-blue.min.css',
         'https://fonts.googleapis.com/css?family=Abel',
         'https://fonts.googleapis.com/icon?family=Material+Icons',
-        '/pizzas.json',
-        changeImageUrl('http://www.nasha-pizza.by/components/com_jshopping/files/img_products/flor.jpg')
+        '/pizzas.json'
       ]).then(function() {
         return self.skipWaiting();
       });
@@ -35,16 +34,9 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-        return fetch(changeImageUrl(event.request.url));
+        return fetch(event.request);
       }
     )
   );
 });
 
-
-function changeImageUrl(url)
-{
-    var newUrl = url.replace(new RegExp("/",'g'),"-").replace(new RegExp("http:--",'g'),"/Images/");
-    console.log(newUrl);
-    return newUrl;
-}
