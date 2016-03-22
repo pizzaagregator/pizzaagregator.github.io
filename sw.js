fcfb84be06d46ba85eedba91ea126b5c2f6e5bd4
Dzmitry Matus <dzmitry_matus@epam.com>
2016-03-22 12:25:27 +0300

    update service worker

--- sw.js
+++ sw.js
@@ -15,8 +15,13 @@ self.addEventListener('install', function(e)
                     '/Content/styles/load-cube.css',
                     '/Content/styles/material.brown-blue.min.css',
                     '/Content/scripts/material.min.js',
+                    '/Content/scripts/jquery-2.1.4.min.js',
+                    '/Content/scripts/jquery.tmpl.min.js',
+                    '/Content/scripts/jquery.twbsPagination.min.js',
+                    '/Content/scripts/index.js',
                     'https://fonts.googleapis.com/css?family=Abel',
                     'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://fonts.gstatic.com/s/abel/v6/brdGGFwqYJxjg2CD1E9o7g.woff2',
                     '/pizzas.json'
                 ])
             }));
